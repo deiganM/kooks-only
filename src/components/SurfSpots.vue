114bba397eab05f540d57f0f7577fe72666b117a
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2021-07-21 23:01:56 -0600

    feat: hooked up surf.json

--- src/components/SurfSpots.vue
+++ src/components/SurfSpots.vue
@@ -2,33 +2,127 @@
   <div class="main-container">
     <div class="surf-spot-container">
       <div class="location-container">
-        <p>Long Beach</p>
+        <h3>{{longBeach.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{longBeach.swell}}</b>
       </div>
       <div class="location-container">
-        <p>Chesterman Beach (North)</p>
+        <h3>{{chestermanN.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{chestermanN.swell}}</b>
       </div>
       <div class="location-container">
-        <p>Chesterman Beach (South)</p>
+        <h3>{{chestermanS.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{chestermanS.swell}}</b>
       </div>
       <div class="location-container">
-        <p>Wickaninnish</p>
+        <h3>{{wickaninnish.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{wickaninnish.swell}}</b>
       </div>
       <div class="location-container">
-        <p>Cox Bay</p>
+        <h3>{{coxBay.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{coxBay.swell}}</b>
       </div>
       <div class="location-container">
-        <p>Sombrio</p>
+        <h3>{{Sombrio.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{Sombrio.swell}}</b>
       </div>
       <div class="location-container">
-        <p>Jordan River</p>
+        <h3>{{jordanRiver.title}}</h3>
+        <p>Swell Size</p>
+        <b>{{jordanRiver.swell}}</b>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  name: 'SurfSpots',
+  data() {
+    return{
+      spots: [],
+      longBeach: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+      chestermanN: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+      chestermanS: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+      wickaninnish: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+      coxBay: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+      Sombrio: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+      jordanRiver: {
+        id:'',
+        title:'',
+        swell:'',
+      },
+    }
+  },
+  mounted () {
+    this.fetchApi()
+  },
+  methods: {
+    async fetchApi() {
+      await axios.get('http://localhost:3000/spots') 
+      .then((response)=>{
+        //Long Beach
+        this.longBeach.id = response.data[0].id,
+        this.longBeach.swell = response.data[0].swell,
+        this.longBeach.title = response.data[0].title
+        //Chesterman Beach (North)
+        this.chestermanN.id = response.data[1].id,
+        this.chestermanN.swell = response.data[1].swell,
+        this.chestermanN.title = response.data[1].title
+        //Chesterman Beach (South)
+        this.chestermanS.id = response.data[2].id,
+        this.chestermanS.swell = response.data[2].swell,
+        this.chestermanS.title = response.data[2].title
+        //Wickaninnish
+        this.wickaninnish.id = response.data[3].id,
+        this.wickaninnish.swell = response.data[3].swell,
+        this.wickaninnish.title = response.data[3].title
+        //Cox Bay
+        this.coxBay.id = response.data[4].id,
+        this.coxBay.swell = response.data[4].swell,
+        this.coxBay.title = response.data[4].title
+        //Cox Bay
+        this.Sombrio.id = response.data[5].id,
+        this.Sombrio.swell = response.data[5].swell,
+        this.Sombrio.title = response.data[5].title
+        //Jordan River
+        this.jordanRiver.id = response.data[6].id,
+        this.jordanRiver.swell = response.data[6].swell,
+        this.jordanRiver.title = response.data[6].title
+      })
+    }
+  }
 }
 </script>
 
@@ -49,8 +143,8 @@ export default {
 }
 
 .location-container {
-  display: flex;
-  justify-content: center;
+  /* display: flex;
+  justify-content: center; */
   border: solid rgb(51, 66, 131) 2px;
   height: 200px;
   width: 200px;
